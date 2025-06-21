eff5152dd05a6723a59716a8c4e84e96f2e78bbb
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-06-21 16:42:29 -0300

    Update FormCam.java

--- src/main/java/webcam/FormCam.java
+++ src/main/java/webcam/FormCam.java
@@ -71,9 +71,6 @@ public class FormCAM extends javax.swing.JFrame {
                         }
         
         
-        
                     }
                 }
-                
-            
             }
